f2ec1f061e9bdc21b6321b31c1754ba1f1dc562c
Hiandro Alex <127314813+hiandro6@users.noreply.github.com>
2024-10-28 18:52:02 -0300

    Update app.py (adicionei as rotas que ruan fez)
    reserva de materiais, visualizar aluno e visualizar prof

--- app.py
+++ app.py
@@ -6,10 +6,28 @@ app = Flask(__name__)
 def index():
     return render_template('pages/index.html')
 
+
 @app.route('/inicio')
 def dashboard():
     return render_template('pages/inicio.html')
 
+
 @app.route('/laboratorios')
 def laboratorios():
     return render_template('pages/laboratorios.html')
+
+
+@app.route('/reserva_materiais')
+def reserva_materiais():
+    return render_template('pages/reserva_materiais.html')
+
+
+@app.route('/visualizar_prof')
+def visualizar_prof():
+    return render_template('pages/visualizar_prof.html')
+
+
+@app.route('/visualizar_aluno')
+def visualizar_aluno():
+    return render_template('pages/visualizar_aluno.html')
+
