2986e194631051a17dcd2070dc9d54c0136e5195
hiandro6 <hiandroalex6@gmail.com>
2024-10-28 23:31:56 -0300

    adicionei o método post nas rotas que fiz, só de boas

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -32,11 +32,21 @@ def visualizar_aluno():
     return render_template('pages/visualizar_aluno.html')
 
 
-@app.route('/cadastrar_material')
+@app.route('/cadastrar_material', methods=['POST', 'GET'])
 def cadastrar_material():
+    if request.method == 'POST':
+        nome_material = request.form['nome_material']
+        tipo_material = request.form['tipo_material']
+        return f"<h1> metodo post nome: {nome_material} tipo: {tipo_material}</h1>"
     return render_template('pages/cadastrar_material.html')
 
 
-@app.route('/cadastrar_lab')
+@app.route('/cadastrar_lab', methods=['POST', 'GET'])
 def cadastrar_lab():
+    if request.method == 'POST':
+        nome_lab = request.form['nome_lab']
+        especialidade_lab = request.form['especialidade_lab']
+        local_lab = request.form['local_lab']
+        capacidade_lab = request.form['capacidade_lab']
+        return f"<h1> metodo post nome: {nome_lab} especialidade: {especialidade_lab} local: {local_lab} quantidade: {capacidade_lab}</h1>"
     return render_template('pages/cadastrar_lab.html')
