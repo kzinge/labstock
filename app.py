81bb182f907da9f4b73c4d046de68244c5f1ccf9
hiandro6 <hiandroalex6@gmail.com>
2025-02-06 09:53:34 -0300

    organização dos arquivos lab

--- app.py
+++ app.py
@@ -85,16 +85,6 @@ def cadastrar_material():
     return render_template('pages/cadastrar_material.html')
 
 
-@app.route('/cadastrar_lab', methods=['POST', 'GET'])
-def cadastrar_lab():
-    if request.method == 'POST':
-        nome_lab = request.form['nome_lab']
-        especialidade_lab = request.form['especialidade_lab']
-        local_lab = request.form['local_lab']
-        capacidade_lab = request.form['capacidade_lab']
-        return f"<h1> metodo post nome: {nome_lab} especialidade: {especialidade_lab} local: {local_lab} quantidade: {capacidade_lab}</h1>"
-    return render_template('pages/cadastrar_lab.html')
-
 @app.route('/logout', methods=['POST'])
 def logout():
     logout_user()
--- controllers/laboratorios.py
+++ controllers/laboratorios.py
@@ -1,6 +1,7 @@
 #Rotas para laboratórios
-from flask import Blueprint, render_template, redirect, url_for
-
+from flask import Blueprint, render_template, request, redirect, url_for
+from ..models.laboratorios import Lab, ReservaLab
+from ..database import *
 lab_bp = Blueprint(name ='lab', 
                     import_name= __name__, 
                     url_prefix='/lab', 
@@ -9,3 +10,14 @@ lab_bp = Blueprint(name ='lab',
 @lab_bp.route('/')
 def index():
     return 'laboratorios'
+
+@lab_bp.route('/cadastrar', methods=['POST', 'GET'])
+def cadastrar_lab():
+    if request.method == 'POST':
+        nome_lab = request.form['nome_lab']
+        especialidade_lab = request.form['especialidade_lab']
+        local_lab = request.form['local_lab']
+        capacidade_lab = request.form['capacidade_lab']
+        laboratorio = Lab(nome_lab, especialidade_lab, local_lab, capacidade_lab)
+        return f"<h1> metodo post nome: {nome_lab} especialidade: {especialidade_lab} local: {local_lab} quantidade: {capacidade_lab}</h1>"
+    return render_template('laboratorios/cadastrar_lab.html')
