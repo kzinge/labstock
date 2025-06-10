0097d2d396e3b38f8374cc9378db8b2287a594d6
Ítalo Lima Verde <20221101110079@ifrn.local>
2025-06-10 11:17:56 -0300

    cadastro especialidadelab e lógica das categorias arrumadas

--- src/app/controllers/tecnico/tecnico.py
+++ src/app/controllers/tecnico/tecnico.py
@@ -4,6 +4,7 @@ from flask_login import current_user
 from ...database import db
 from ...models import User
 from ..material.actions.services import materialservice
+from ..laboratorio.actions.services import labservice
 from ...models import CategoriaMaterial, CategoriaReagente, EspecialidadeLab
 
 tec_bp = Blueprint(name ='tecnico', 
@@ -11,7 +12,7 @@ tec_bp = Blueprint(name ='tecnico',
                     url_prefix='/tecnico', 
                     template_folder='pages')
 
-def nova_categoria(form):
+def criar_categoria(form):
     if form['tipo'] == "material":
         nome = form['nome_categoria']
         categorias = materialservice.get_categorias_materiais()
@@ -19,9 +20,11 @@ def nova_categoria(form):
             for categoria in categorias:
                 if nome == categoria.cat_nome:
                     flash('Categoria já existe')
+                    cat_existente = True
+
                     return redirect(url_for('material.cadastrar_categoria'))
-                else:    
-                    nova_categoria = CategoriaMaterial(nome)
+            if not cat_existente:    
+                nova_categoria = CategoriaMaterial(nome)
         else:    
             nova_categoria = CategoriaMaterial(nome)
 
@@ -34,12 +37,30 @@ def nova_categoria(form):
                 if nome == categoria.cat_nome:
                     print('CATEGORIA JA EXISTE')
                     flash('Categoria já existe')
+                    cat_existente = True
+
                     return redirect(url_for('material.cadastrar_categoria'))
-                else:    
-                    nova_categoria = CategoriaReagente(form['nome_categoria'])
+            if not cat_existente:    
+                nova_categoria = CategoriaReagente(form['nome_categoria'])
         else:    
             nova_categoria = CategoriaReagente(form['nome_categoria'])
+        
+    elif form['tipo'] == "lab":
+        nome = form["nome_categoria"]
+        categorias = labservice.listar_especialidades()
+        cat_existente = False
+        for categoria in categorias:
+            if nome == categoria.esp_nome:
+                flash('Categoria já existe')
+                cat_existente = True
 
+                return redirect(url_for('tecnico.cadastrar_categoria'))
+                
+        if not cat_existente: 
+            nova_categoria = EspecialidadeLab(esp_nome = form['nome_categoria'])
+    
+    else:
+        return "erro"
     db.session.add(nova_categoria)
     db.session.commit()
 
@@ -52,8 +73,6 @@ def cadastrar_categoria():
     if request.method == 'GET':
         return render_template('/pages/categoria.html')
     else:
-        nova_categoria(request.form)
+        criar_categoria(request.form)
         flash('Categoria criada com sucesso!')
         return redirect(url_for('tecnico.cadastrar_categoria'))
-
-
