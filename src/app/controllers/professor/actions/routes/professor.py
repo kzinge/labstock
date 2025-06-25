582767731d0154c90613a9f9789c63aba42646d8
italolverde <italolverde3@gmail.com>
2025-06-25 11:16:33 -0300

    quantidade real de materiais em /professor, comentários pra depois

--- src/app/controllers/professor/actions/routes/professor.py
+++ src/app/controllers/professor/actions/routes/professor.py
@@ -1,8 +1,18 @@
 from ... import prof_bp
 from ..services.user import find_user
 from flask import render_template, redirect, url_for
+from .....models import Material, Reagente, CategoriaReagente, CategoriaMaterial, ReservaItens, ReservaMaterial, ReservaReagente
 
 @prof_bp.route('/')
 def home():
+    materiais_cadastrados = Material.query.count()
+    reagentes_cadastrados = Reagente.query.count()
+    itens_cadastrados = materiais_cadastrados + reagentes_cadastrados
+
+    #Pegar reservas filtradas por id do usuário current_user
+    #Separar as reservas entre soliciadas, pendentes e confirmadas
+    #Pegar quantidade de aulas cadastradas ( Fazer as aulas primeiro )
+    
+
     usuario = find_user()
-    return render_template('home.html', nome = usuario.usu_nome)
+    return render_template('home.html', nome = usuario.usu_nome, quantidade_material = itens_cadastrados)
